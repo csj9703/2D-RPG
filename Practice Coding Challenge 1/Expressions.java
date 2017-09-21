d19554329b6b4419520ced1c55f7bdeb7beecf83
eddy <eddy@ualberta.ca>
2017-09-20 23:38:07 -0600

    Practice Coding Challenge 1

--- Practice Coding Challenge 1/Expressions.java
+++ Practice Coding Challenge 1/Expressions.java
@@ -1,12 +1,12 @@
-/**
+/*
  *  Practice Coding Challenge 1
  */
 
 public class Expressions 
 {
     /**
-	 * This method accepts a double and returns its floor value
-	 * @param The double to be calculated
+	 * This method accepts a number and returns its floor value
+	 * @param num The double to be calculated
 	 * @return The floor value as a long
 	 */
 
@@ -20,18 +20,18 @@ public class Expressions
     /**
 	 * This method converts fahrenheit to celcius
 	 * @param The fahrenheit value as a double to be converted
-	 * @return degrees celcius as a double
+	 * @return Degrees celcius as a double
 	 */
 
 	//method 2
 	public static double conversion(double num)
 	{
-		return (((num - 32)*5)/9);
+		return ((num - 32)*5)/9;
 	}
 	
 
     /**
-	 * This method checks wether the passed double will be rounded
+	 * This method checks whether the passed number will be rounded up
 	 * @param The double being checked
 	 * @return True when rounds up, false otherwise
 	 */
@@ -39,13 +39,11 @@ public class Expressions
 	//method 3
 	public static boolean willRoundUp(double num)
 	{
-		long wholePart = (long) num;
-		double decimalPart = num - wholePart;
-		return !(decimalPart < 0.5);
+		return !(num - (long) num < 0.5);
 	}
 	
     /**
-	 * This method sums the digits of passed integer
+	 * This method sums the last five digits of an integer
 	 * @param The integer being summed
 	 * @return The sum of digits of the integer
 	 */
@@ -53,14 +51,11 @@ public class Expressions
 	//method 4
 	public static int addDigits(int num)
 	{
-
-        // Gets last five digits
-        num %= 100000;
-        
-        return (num / 10000)        // first digit
-		     + (num / 1000) % 10    // second digit
-		     + (num / 100) % 10     // third digit
-		     + (num / 10) % 10      // fourth digit
-		     + (num / 1) % 10;      // fifth digit		
+        num %= 100000;         // gets last five digits
+        return (num / 10000)   // first digit
+		+ (num / 1000) % 10    // second digit
+		+ (num / 100) % 10     // third digit
+		+ (num / 10) % 10      // fourth digit
+		+ (num / 1) % 10;      // fifth digit		
 	}
 }
