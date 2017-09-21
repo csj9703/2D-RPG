6d785ee44500647d4dc04050b078a72ce39e2f61
eddy <eddy@ualberta.ca>
2017-09-21 11:27:09 -0600

    Coding Practice Challenge 0

--- Practice Coding Challenge 0/Change.java
+++ Practice Coding Challenge 0/Change.java
@@ -15,30 +15,30 @@ public class Change
 	// This method returns the number of loonies in change
 	public static int getNumOfLoonies(int changeNeeded)
 	{
-		return changeNeeded = (changeNeeded % 200) / 100;
+		return (changeNeeded % 200) / 100;
 	}
 	
 	// This method returns the number of quarters in change
 	public static int getNumOfQuarters(int changeNeeded) 
 	{
-		return changeNeeded = ((changeNeeded % 200) % 100) / 25;
+		return ((changeNeeded % 200) % 100) / 25;
 	}
 	
 	// This method returns the number of dimes in change
 	public static int getNumOfDimes(int changeNeeded)
 	{
-		return changeNeeded = (((changeNeeded % 200) % 100) % 25) / 10;
+		return (((changeNeeded % 200) % 100) % 25) / 10;
 	}
 	
 	// This method returns the number of nickels in change
 	public static int getNumOfNickels(int changeNeeded)
 	{
-		return changeNeeded = ((((changeNeeded % 200) % 100) % 25) % 10) / 5;
+		return ((((changeNeeded % 200) % 100) % 25) % 10) / 5;
 	}
 	
 	// This method returns the number of pennies in change
 	public static int getNumOfPennies(int changeNeeded)
 	{
-		return changeNeeded = (((((changeNeeded % 200) % 100) % 25) % 10) % 5);		
+		return (((((changeNeeded % 200) % 100) % 25) % 10) % 5);		
 	}
 }
