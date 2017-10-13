e87981402e3942aeebc138302235e9f7db203786
eddy <eddy@ualberta.ca>
2017-10-12 23:20:42 -0600

    1.5

--- Assignment 3/BankAccount.java
+++ Assignment 3/BankAccount.java
@@ -1,8 +1,8 @@
 /**
  * This class contains the methods of a bank account
  * @author Team 6 - CPSC 233 - Tutorial T02
- * @version 1.4
- * @since October 11, 2017
+ * @version 1.5
+ * @since October 12, 2017
  */
 
 public class BankAccount 
@@ -13,6 +13,7 @@ public class BankAccount
 	/**
 	 * This constructor takes an variable of the 
 	 * Customer type as an argument
+	 * @param customer The customer object
 	 */
 	public BankAccount(Customer customer)
 	{
@@ -22,6 +23,8 @@ public class BankAccount
 	/**
 	 * This constructor takes an variable of the 
 	 * Customer type and a double as an argument
+	 * @param initialBalance The starting balance of the bank account
+	 * @param customer  An instance of the customer class
 	 */
 	public BankAccount(double initialBalance, Customer customer)
 	{
@@ -31,6 +34,7 @@ public class BankAccount
 	
 	/**
 	 * A copy constructor. 
+	 * @param account The BankAccount object to be copied
 	 */
 	public BankAccount(BankAccount account)
 	{
@@ -83,8 +87,12 @@ public class BankAccount
 		}
 	}
 	
+	/**
+	 * Getter method for the account holder
+	 * @return accountHolder The account holder
+	 */
 	public Customer getAccountHolder()
 	{
-		return accountHolder;
+		return new Customer(accountHolder);
 	}
 }
