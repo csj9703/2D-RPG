8b1cffea939a9782b5581b595617e4f7f5b9ce7b
Krums <darkdavris123@gmail.com>
2017-11-02 18:05:38 -0600

    Testing Getter

--- assignments/Assignment 6/TriangleTest.java
+++ assignments/Assignment 6/TriangleTest.java
@@ -39,4 +39,51 @@ public class TriangleTest
 						                 new Line(new Point(3158,64081), new Point(1,1)));
 		assertEquals("Circumferance should be 135988.39348", 135988.39348, triangle.getCircumference(), 0.00001);
 	}
+	
+	//Jason
+	@Test
+	public void test_getLine1_triangle()
+	{
+		Triangle triangle = new Triangle(new Line(new Point(0,0), new Point(2,0)),
+                new Line(new Point(2,0), new Point(0,2)),
+                new Line(new Point(0,2), new Point(0,0)));
+		
+		assertEquals("Line 1 length should be 2.0",2.0, triangle.getLine1().length(), 0.00001);
+	}
+	
+	@Test
+	public void test_getLine1_triangle_privacyLeak()
+	{
+		Line l1 = new Line(new Point(0,0), new Point(2,0));
+		
+		Triangle triangle = new Triangle(l1,
+                new Line(new Point(2,0), new Point(0,2)),
+                new Line(new Point(0,2), new Point(0,0)));
+		Line original = triangle.getLine1();
+		l1 = new Line(new Point(1,0), new Point(2,0));
+				
+		assertEquals("Line 1 length should be 2.0",original,triangle.getLine1());
+	}
+	
+	@Test
+	public void test_getLine2_triangle()
+	{
+		Triangle triangle = new Triangle(new Line(new Point(0,0), new Point(2,0)),
+                new Line(new Point(0,0), new Point(2,0)),
+                new Line(new Point(0,2), new Point(0,0)));
+		
+		assertEquals("Line 2 length should be 2.0",2.0, triangle.getLine2().length(), 0.00001);
+	}
+	
+	@Test
+	public void test_getLine3_triangle()
+	{
+		Triangle triangle = new Triangle(new Line(new Point(0,0), new Point(2,0)),
+                new Line(new Point(2,0), new Point(0,2)),
+                new Line(new Point(0,0), new Point(2,0)));
+		
+		assertEquals("Line 3 length should be 2.0",2.0, triangle.getLine3().length(), 0.00001);
+	}
+	
+	
 }
